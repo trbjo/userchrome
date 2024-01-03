f4c605ceb588a568a1bdd93601d9081efc83c3f9
Troels Bjørnskov <troels@bjoernskov.org>
2024-01-03 11:56:04 +0100

    updates

--- userChrome.js
+++ userChrome.js
@@ -1,9 +1,13 @@
-/// Create in Firefox-Install-Directory/UserChrome.js - a minimal bootstrap to run js snippets on startup - AveYo, 2023.09.14
-/// Requires: Firefox-Install-Directory/defaults/pref/enable-UserChrome.js
-
 let { classes: Cc, interfaces: Ci, manager: Cm, utils: Cu } = Components;
 const Services = globalThis.Services || Components.utils.import("resource://gre/modules/Services.jsm").Services;
 const { E10SUtils } = ChromeUtils.import("resource://gre/modules/E10SUtils.jsm");
+const { Downloads } = ChromeUtils.importESModule("resource://gre/modules/Downloads.sys.mjs");
+
+
+const { showAlertNotification } = Cc["@mozilla.org/alerts-service;1"].getService(
+  Ci.nsIAlertsService
+);
+const MOZSTARTUPTIME = Date.now();
 
 let xP = Services.prefs, xD = xP.getDefaultBranch(null), xZ = void 0, xS = 'string', xN = 'number', xB = 'boolean', xPref = {
   get: function (e, t = !1, r, n = !0) {
@@ -19,16 +23,213 @@ let xP = Services.prefs, xD = xP.getDefaultBranch(null), xZ = void 0, xS = 'stri
   removeListener: function (e) { xP.removeObserver(e.p, e.o) }
 }; /// uncomment to use minified xPref.jsm
 
+globalThis.RunningPIDs = new Set();
+globalThis.globalActiveDownload = false;
+globalThis.ForegroundPIDs = new Set();
+globalThis.INActivePIDs = new Set();
+globalThis.presetTitle = "";
+
+
+function browserWindows() {
+  return Services.wm.getEnumerator("navigator:browser");
+}
+
+
+function setTitle({ runningPIDs = globalThis.RunningPIDs, foregroundPIDs = globalThis.ForegroundPIDs, inactivePIDs = globalThis.INActivePIDs } = {}) {
+  const divider = "=|=";
+  let titleString = divider;
+
+  titleString += Uint32Array.from([globalThis.globalActiveDownload, ...runningPIDs]).sort().join(",");
+  titleString += divider;
+  titleString += Uint32Array.from(foregroundPIDs).sort().join(",");
+  titleString += divider;
+  titleString += Uint32Array.from(inactivePIDs).sort().join(",");
+  titleString += divider;
+  for (let win of browserWindows()) {
+    win.document.title = titleString;
+  }
+  globalThis.presetTitle = titleString;
+  globalThis.RunningPIDs = runningPIDs;
+  globalThis.ForegroundPIDs = foregroundPIDs;
+  globalThis.INActivePIDs = inactivePIDs;
+}
+
+// listen for active downloads
+(async () => {
+  const list = await Downloads.getList(Downloads.ALL);
+  const myDownloadView = {
+    downloadId(download) {
+      return download.target.path + download.startTime.toString(36)
+    },
+    _downloadsInProgress: new Map(),
+    imageURL: "chrome://branding/content/icon128.png",
+    extractFilename(download) {
+      const fullPath = download.target.path
+      var pathSep = '/';    // Linux / (probably) OSX
+      var filepath = fullPath.split(pathSep);
+      var filename = filepath.pop(filepath.length - 1);
+      return filename
+    },
+    notify(title, message) {
+      showAlertNotification(this.imageURL, title, message);
+    },
+    onDownloadAdded(download) {
+      const body = this.extractFilename(download);
+      var title;
+      if ((Date.now() - 2000) > MOZSTARTUPTIME) {
+        title = "Download Added";
+      } else {
+        title = "Download Resumed";
+      }
+      this._downloadsInProgress.set(this.downloadId(download), download);
+      this.notify(title, body);
+      globalThis.globalActiveDownload = this._downloadsInProgress.size > 0;
+      setTitle();
+    },
+    onDownloadChanged(download) {
+      var title;
+      if (download.succeeded === true) {
+        title = 'Download Completed';
+      } else if (download.canceled) {
+        title = 'Download Cancelled';
+      } else if (download.error) {
+        title = 'Download Failed';
+      } else {
+        return
+      }
+      const body = this.extractFilename(download);
+      this._downloadsInProgress.delete(this.downloadId(download));
+      globalThis.globalActiveDownload = this._downloadsInProgress.size > 0;
+      this.notify(title, body);
+      setTitle();
+    },
+    onDownloadRemoved(download) {
+      this._downloadsInProgress.delete(this.downloadId(download));
+      globalThis.globalActiveDownload = this._downloadsInProgress.size > 0;
+      setTitle();
+    }
+  };
+  await list.addView(myDownloadView);
+})();
+
+
 let UserChrome_js = {
   observe: function (s) {
     s.addEventListener('DOMContentLoaded', this, { once: true });
   },
 
   handleEvent: async function (evt) {
-    let browser = evt.originalTarget, document = browser, window = browser.defaultView, console = window.console;
+    const root = evt.originalTarget;
+    let window = root.defaultView;
+    let console = window.console;
+    const closeWindow = (event) => {
+      if (event && event.target && event.target.gBrowser) {
+        var tabPIDs = new Set(event.target.gBrowser.tabs.reduce((acc, tab) => acc.concat(getTabPids(tab)), []));
+        promoteTab(tabPIDs);
+      }
+
+      for (let window of browserWindows()) {
+        window.setTimeout(updatePIDsAndSetTitle, 300);
+      }
+    }
+    const promoteTab = (tabPids) => {
+      // ensures the tab is unsuspended
+      const runningPIDs = new Set([...globalThis.RunningPIDs, ...tabPids])
+      const foregroundPIDs = new Set([...globalThis.ForegroundPIDs].filter(x => !runningPIDs.has(x)));
+      const maxActive = new Set([...runningPIDs, ...foregroundPIDs])
+      const inactivePIDs = new Set([...globalThis.INActivePIDs].filter(x => !maxActive.has(x)));
+      setTitle({ runningPIDs: runningPIDs, foregroundPIDs: foregroundPIDs, inactivePIDs: inactivePIDs });
+    }
+    const getTabPids = (tab) => {
+      return E10SUtils.getBrowserPids(tab.linkedBrowser, true);
+    }
+
+    var intervalId = null;
+    var timer = null;
+
+    const updatePIDsAndSetTitle = (event) => {
+      let runningPIDs = new Set();
+      let foregroundPIDs = new Set();
+      let inactivePIDs = new Set();
+
+      let tabs = [];
+      for (let win of browserWindows()) {
+        tabs.push(...win.gBrowser.tabs)
+      }
+      var tabMightChangePID = false;
+      for (let tab of tabs) {
+        var tabPids = getTabPids(tab);
+        if (tab.hasAttribute("busy")) {
+          tabPids.forEach(pid => runningPIDs.add(pid));
+          tabMightChangePID = true;
+        } else if ((tab.soundPlaying && !tab.hasAttribute("soundplaying-scheduledremoval")) || tab.pictureinpicture === true) {
+          tabPids.forEach(pid => runningPIDs.add(pid));
+        } else if (tab.selected === true) {
+          tabPids.forEach(pid => foregroundPIDs.add(pid));
+        } else if (tab.isEmpty === true) {
+          tabPids.forEach(pid => foregroundPIDs.add(pid));
+          // "New Tab"
+        } else if (tab.hasAttribute("pending") === true) {
+          tabPids.forEach(pid => foregroundPIDs.add(pid));
+          // tab has not been loaded yet
+        } else {
+          tabPids.forEach(pid => inactivePIDs.add(pid));
+        }
+      }
+
+      // cleanup: some tabs share pids, we prefer the active ones.
+      runningPIDs.forEach(pid => inactivePIDs.delete(pid));
+      runningPIDs.forEach(pid => foregroundPIDs.delete(pid));
+      foregroundPIDs.forEach(pid => inactivePIDs.delete(pid));
+
+      // window close
+      if (typeof event === 'undefined') {
+        setTitle({ runningPIDs: runningPIDs, foregroundPIDs: foregroundPIDs, inactivePIDs: inactivePIDs });
+        return
+      }
+
+      if ((tabMightChangePID === true) && (intervalId === null)) {
+        intervalId = window.setInterval(() => {
+          var tabs = [];
+          for (let win of browserWindows()) {
+            tabs.push(...win.gBrowser.tabs)
+          }
+          var busyTabs = tabs.filter(tab => tab.hasAttribute("busy"));
+          var newBusyPids = new Set(busyTabs.reduce((acc, tab) => acc.concat(getTabPids(tab)), []));
+          promoteTab(newBusyPids);
+          if (newBusyPids.size === 0) {
+            window.clearInterval(intervalId);
+            intervalId = null;
+          }
+        }, 50);
+      }
+
+      // keep the previously open tabs for a while
+      const runningPIDsUnion = new Set([...globalThis.RunningPIDs, ...runningPIDs])
+      const ForegroundPIDsUnion = new Set([...globalThis.ForegroundPIDs, ...foregroundPIDs].filter(x => !runningPIDsUnion.has(x)));
+      const inactivePIDsIntersection = new Set([...globalThis.INActivePIDs].filter(x => inactivePIDs.has(x)));
+      setTitle({ runningPIDs: runningPIDsUnion, foregroundPIDs: ForegroundPIDsUnion, inactivePIDs: inactivePIDsIntersection });
+
+      if (timer !== null) {
+        window.clearTimeout(timer);
+      }
+      timer = window.setTimeout(setTitle, 500, { runningPIDs: runningPIDs, foregroundPIDs: foregroundPIDs, inactivePIDs: inactivePIDs });
+    }
 
     if (window.gBrowserInit && !window.gBrowserInit.delayedStartupFinished) {
       await window.delayedStartupPromise;
+    }
+
+    const winType = evt.originalTarget.location.href;
+    if (winType === "chrome://global/content/pictureinpicture/player.xhtml") {
+      closeWindow();
+      window.addEventListener("close", closeWindow);
+      window.addEventListener("click", closeWindow);
+    }
+
+    if (winType !== "chrome://browser/content/browser.xhtml") {
+      console.log(evt)
+      return
     }
 
     if (!window.gBrowserInit || !window.docShell) {
@@ -45,97 +246,60 @@ let UserChrome_js = {
     // @onlyonce
     // ==/UserScript==
 
-    if (typeof UC === 'undefined') UC = {};
+    const { document, gBrowser } = window;
+    let tabContainer = gBrowser.tabContainer;
 
-    UC.TabPIDsInWindowTitle = {
-      init: function () {
-        var { document, gBrowser } = window;
-        let tabContainer = gBrowser.tabContainer;
-        const divider = "=|=";
-        const windowSUFFIX = "Mozilla Firefox";
-
-        function updatePIDsAndSetTitle() {
-          let activePIDs = new Set();
-          let runningPIDs = new Set();
-          let inActivePIDs = new Set();
-          let allTabs = gBrowser.visibleTabs;
-
-          for (var i = 0; i < allTabs.length; i++) {
-            tab = allTabs[i];
-            let { linkedBrowser } = tab;
-            var tabPids = E10SUtils.getBrowserPids(linkedBrowser, true);
-            if (tab.soundPlaying) {
-              tabPids.forEach(pid => runningPIDs.add(pid));
-            } else if (tab.selected === true || tab.hasAttribute("busy")) {
-              tabPids.forEach(pid => activePIDs.add(pid));
-            } else {
-              tabPids.forEach(pid => inActivePIDs.add(pid));
-            }
-          };
-
-          // some pids are in both:
-          activePIDs.forEach(pid => inActivePIDs.delete(pid));
-          runningPIDs.forEach(pid => inActivePIDs.delete(pid));
-
-          let titleString = divider;
-          titleString += Array.from(runningPIDs).join(",");
-          titleString += divider;
-          titleString += Array.from(activePIDs).join(",");
-          titleString += divider;
-          titleString += Array.from(inActivePIDs).join(",");
-          titleString += divider;
-          document.title = titleString;
-        }
-
-
-
-        // Callback function to execute when mutations are observed
-        const callback = (mutationList, observer) => {
-          for (const mutation of mutationList) {
-            let text = mutation.target.innerText;
-            if (text.endsWith(windowSUFFIX)) {
-              updatePIDsAndSetTitle();
-            }
+    const titleCallback = (mutationList, observer) => {
+      const windowSUFFIX = "Mozilla Firefox";
+      for (const mutation of mutationList) {
+        let text = mutation.target.innerText;
+        if (text.endsWith(windowSUFFIX)) {
+          for (let win of browserWindows()) {
+            win.document.title = globalThis.presetTitle;
           }
-        };
-
-        function logEvent(event) {
-          console.log("////////////////////////////////////")
-          console.log(event)
-          console.log(event.type, event.target)
-          console.log("////////////////////////////////////")
+          return
         }
-
-        function updateStateAndSetTitle(event) {
-          updatePIDsAndSetTitle();
-        }
-
-        const listeners = [
-          // "TabSelect",
-          // "TabClose",
-          "TabAttrModified",
-          "SSTabRestored",
-          // "TabHide",
-          // "TabShow",
-          // TabOpen,
-          // "TabPinned",
-          // "TabUnpinned",
-          // "transitionend",
-        ]
-        // window.addEventListener("deactivate", lol);
-
-        listeners.forEach(event => {
-          tabContainer.addEventListener(event, updateStateAndSetTitle, false);
-        });
-
-        const observer = new window.MutationObserver(callback);
-        const titleNode = document.head.querySelector('title');
-        const config = { attributes: true, childList: true, subtree: true };
-        observer.observe(titleNode, config);
-        console.info('\u2713 TabPIDsInWindowTitle');
       }
     };
-    UC.TabPIDsInWindowTitle.init();
+
+    const afterClose = (event) => {
+      tab = event.target;
+      if (tab.collapsed === true || !tab.linkedBrowser) {
+        updatePIDsAndSetTitle("delay");
+      }
+    }
+
+    const closeTab = (event) => {
+      tab = event.target;
+      let tabPIDs = getTabPids(tab);
+      promoteTab(tabPIDs);
+    }
+
+    const titleObserver = new window.MutationObserver(titleCallback);
+    const titleNode = document.head.querySelector('title');
+    const config = { attributes: true, childList: true, subtree: true };
+    titleObserver.observe(titleNode, config);
+
+    const listeners = [
+      // "TabSelect",
+      // "TabClose",
+      "TabAttrModified",
+      "SSTabRestored",
+      // "TabHide",
+      // "TabShow",
+      // "TabOpen",
+      // "TabPinned",
+      // "TabUnpinned",
+      // "transitionend",
+    ]
+    listeners.forEach(event => {
+      tabContainer.addEventListener(event, updatePIDsAndSetTitle, false);
+    });
+    tabContainer.addEventListener("TabClose", closeTab, false);
+    tabContainer.addEventListener("transitionend", afterClose, false);
+    window.addEventListener("close", closeWindow);
+
+    console.info('\u2713 TabPIDsInWindowTitle');
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
